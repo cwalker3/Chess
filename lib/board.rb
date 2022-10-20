b1e8645ba0aabe59092a88a379e1ec7498d33e39
Cory Walker <tepoen98@gmail.com>
2022-10-19 18:24:06 -0700

    add create_board and populate_board methods

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,21 @@
 
 # board class for chess
 class Board
-  def initialize
-    
+  def initialize(board = create_board)
+    @board = board
   end
+
+  def create_board
+    board = Array.new(8) { Array.new(8) }
+    populate_board
+  end
+
+  def populate_board
+    board.each_index do |row|
+      row.each_index do |column|
+        board[row][column] << Piece.for([row, column])
+      end
+    end
+  end
+
 end
